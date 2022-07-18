c907527c685767f2fb6f1dabb11fbd668abb003a
skyv26 <dev.aakashv@gmail.com>
2022-07-18 19:54:54 +0530

    update files

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let mobileMenu = document.querySelector('.menu-btn--icon');
-let mobileNav = document.querySelector('.main-header__navigation--menu__list');
+let mobileNavList = document.querySelector('.main-header__navigation--menu__list');
+let mobileNav = document.querySelector('.main-header__navigation--menu');
 
 mobileMenu.addEventListener('click', () => {
   let closeBtn = document.createElement('img');
   closeBtn.src="./assets/IconCancel.svg";
-  closeBtn.style="width: 24px; height: 24px; display: block;"
+  closeBtn.className='menu-btn--close_icon'
   mobileNav.prepend(closeBtn);
   mobileNav.classList.add('mobile-menu');
+  mobileNavList.classList.add('mobile-menu--nav');
+  mobileMenu.style.display='none';
   document.querySelector('body').style.overflow='hidden';
-  console.log(mobileNav.classList);
 })
