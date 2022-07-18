14e6d80a71d513e6ba668e84e02ecfdf9aa23392
John Kibirige <kibirigejohn005@gmail.com>
2022-07-18 18:16:58 +0300

    Add more functionality in the both menu and the close buttons

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-let mobileMenu = document.querySelector('.menu-btn--icon');
-let mobileNavList = document.querySelector('.main-header__navigation--menu__list');
-let mobileNav = document.querySelector('.main-header__navigation--menu');
+let mobileMenu = document.querySelector(".menu-btn--icon");
+let mobileNavList = document.querySelector(
+  ".main-header__navigation--menu__list"
+);
+let mobileNav = document.querySelector(".main-header__navigation--menu");
+const menuItemLinks = document.querySelectorAll(".menu__item--link");
+// const menuItemLink = document.getElementsByClassName("menu__item--link");
+let closeBtn = document.createElement("img");
 
-mobileMenu.addEventListener('click', () => {
-  let closeBtn = document.createElement('img');
-  closeBtn.src="./assets/IconCancel.svg";
-  closeBtn.className='menu-btn--close_icon'
+mobileMenu.addEventListener("click", () => {
+  closeBtn.src = "./assets/IconCancel.svg";
+  closeBtn.className = "menu-btn--close_icon";
   mobileNav.prepend(closeBtn);
-  mobileNav.classList.add('mobile-menu');
-  mobileNavList.classList.add('mobile-menu--nav');
-  mobileMenu.style.display='none';
-  document.querySelector('body').style.overflow='hidden';
-})
+  mobileNav.classList.add("mobile-menu");
+  mobileNavList.classList.add("mobile-menu--nav");
+  mobileMenu.style.display = "none";
+  document.querySelector("body").style.overflow = "hidden";
+  closeBtn.style.display = "block";
+
+  for (let link of menuItemLinks) {
+    link.classList.add("helper-nav-link");
+  }
+});
+
+// adding an event handler to the menuBtnClose
+closeBtn.addEventListener("click", () => {
+  mobileNav.classList.remove("mobile-menu");
+  mobileNavList.classList.remove("mobile-menu--nav");
+  document.querySelector("body").style.overflow = "auto";
+  mobileMenu.style.display = "block";
+  closeBtn.style.display = "none";
+
+  for (let link of menuItemLinks) {
+    link.classList.remove("helper-nav-link");
+  }
+});
