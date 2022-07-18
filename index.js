b41a367de50d86320c5e580fc27c71f29d8e23a9
John Kibirige <kibirigejohn005@gmail.com>
2022-07-18 18:34:12 +0300

    Add linters and resolved linter errors

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-let mobileMenu = document.querySelector(".menu-btn--icon");
-let mobileNavList = document.querySelector(
-  ".main-header__navigation--menu__list"
+const mobileMenu = document.querySelector('.menu-btn--icon');
+const mobileNavList = document.querySelector(
+  '.main-header__navigation--menu__list',
 );
-let mobileNav = document.querySelector(".main-header__navigation--menu");
-const menuItemLinks = document.querySelectorAll(".menu__item--link");
+const mobileNav = document.querySelector('.main-header__navigation--menu');
+const menuItemLinks = document.querySelectorAll('.menu__item--link');
 // const menuItemLink = document.getElementsByClassName("menu__item--link");
-let closeBtn = document.createElement("img");
+const closeBtn = document.createElement('img');
 
-mobileMenu.addEventListener("click", () => {
-  closeBtn.src = "./assets/IconCancel.svg";
-  closeBtn.className = "menu-btn--close_icon";
+mobileMenu.addEventListener('click', () => {
+  closeBtn.src = './assets/IconCancel.svg';
+  closeBtn.className = 'menu-btn--close_icon';
   mobileNav.prepend(closeBtn);
-  mobileNav.classList.add("mobile-menu");
-  mobileNavList.classList.add("mobile-menu--nav");
-  mobileMenu.style.display = "none";
-  document.querySelector("body").style.overflow = "hidden";
-  closeBtn.style.display = "block";
+  mobileNav.classList.add('mobile-menu');
+  mobileNavList.classList.add('mobile-menu--nav');
+  mobileMenu.style.display = 'none';
+  document.querySelector('body').style.overflow = 'hidden';
+  closeBtn.style.display = 'block';
 
-  for (let link of menuItemLinks) {
-    link.classList.add("helper-nav-link");
-  }
+  menuItemLinks.forEach((link) => link.classList.add('helper-nav-link'));
 });
 
 // adding an event handler to the menuBtnClose
-closeBtn.addEventListener("click", () => {
-  mobileNav.classList.remove("mobile-menu");
-  mobileNavList.classList.remove("mobile-menu--nav");
-  document.querySelector("body").style.overflow = "auto";
-  mobileMenu.style.display = "block";
-  closeBtn.style.display = "none";
+closeBtn.addEventListener('click', () => {
+  mobileNav.classList.remove('mobile-menu');
+  mobileNavList.classList.remove('mobile-menu--nav');
+  document.querySelector('body').style.overflow = 'auto';
+  mobileMenu.style.display = 'block';
+  closeBtn.style.display = 'none';
 
-  for (let link of menuItemLinks) {
-    link.classList.remove("helper-nav-link");
-  }
+  menuItemLinks.forEach((link) => link.classList.remove('helper-nav-link'));
 });
