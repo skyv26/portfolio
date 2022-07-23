60b0e019c965a9ddd2efb7cfb0caab18d4b65979
John Kibirige <kibirigejohn005@gmail.com>
2022-07-23 10:57:38 +0300

    Add some logic for the local storage inside index.js file

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const header = document.querySelector('.main-header');
 const workSection = document.querySelector('.work-section_container');
 const form = document.querySelector('.form');
 
+const inputTarget = document.querySelector('.inputs');
+
 const ConvertStringToHTML = (str) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(str, 'text/html');
@@ -23,14 +25,14 @@ header.addEventListener('click', function mobileMenu(e) {
   const nodeElemet = e.target;
   const nodeElementParent = nodeElemet.parentNode.className;
   if (
-    nodeElemet.className.includes('btn--icon')
-    || nodeElemet.className.includes('bar')
-    || nodeElementParent.includes('btn--icon')
+    nodeElemet.className.includes('btn--icon') ||
+    nodeElemet.className.includes('bar') ||
+    nodeElementParent.includes('btn--icon')
   ) {
     this.classList.toggle('mobile-menu--active');
   } else if (
-    nodeElemet.className.includes('menu__item')
-    || nodeElemet.className.includes('menu__item--link')
+    nodeElemet.className.includes('menu__item') ||
+    nodeElemet.className.includes('menu__item--link')
   ) {
     this.classList.remove('mobile-menu--active');
   }
@@ -58,8 +60,8 @@ const featuredProjectHtml = `<div class="work-section_container--highlighted_pro
     </p>
     <ul class="technology-list">
     ${projectList.featuredProject.tags
-    .map((tagName) => `<li class="skills">${tagName}</li>`)
-    .join('')}
+      .map((tagName) => `<li class="skills">${tagName}</li>`)
+      .join('')}
     </ul>
     <a role="button" class="project_list--li__cta featuredProject"
       >See Project</a
@@ -82,13 +84,13 @@ const projectWorkList = projectList.projectGrid.map(
     </ul>
   </div>
   <a role="button" class="project_list--li__cta proj_id-${
-  li.id
-}">See Project</a>
-</li>`,
+    li.id
+  }">See Project</a>
+</li>`
 );
 
 workSectionContainer.appendChild(
-  ...ConvertStringToHTML(featuredProjectHtml).children,
+  ...ConvertStringToHTML(featuredProjectHtml).children
 );
 
 projectWorkList.forEach((eachList) => {
@@ -100,13 +102,15 @@ workSectionContainer.appendChild(projectUnorderedList);
 
 workSection.addEventListener('click', (e) => {
   const getElement = e.target;
-  if (getElement.nodeName.toLowerCase() === 'a' && getElement.getAttribute('role') === 'button') {
+  if (
+    getElement.nodeName.toLowerCase() === 'a' &&
+    getElement.getAttribute('role') === 'button'
+  ) {
     if (getElement.classList[1].includes('proj_id')) {
-      // console.log(getElement.classList[1].split('-')[1]);
       popUpHandler(
         projectList.projectGrid[
           Number(getElement.classList[1].split('-')[1]) - 1
-        ],
+        ]
       );
     } else {
       popUpHandler(projectList[getElement.classList[1]]);
@@ -123,7 +127,7 @@ document.body.addEventListener('click', (e) => {
   }
 });
 
-form.addEventListener('storage', function formSubmitHandler (e) {
+inputTarget.addEventListener('change', function formSubmitHandler(e) {
   const targetElement = e.target;
   const full_name = this.querySelector('input[type="text"]').value;
   const email = this.querySelector('input[type="email"]').value;
@@ -135,10 +139,8 @@ form.addEventListener('storage', function formSubmitHandler (e) {
     message,
   };
 
-  if (targetElement.classList[0] === 'inputs') {
-    console.log(JSON.stringify(contactFormObject));
-    localStorage.setItem('formData', JSON.stringify(contactFormObject));
-  }
+  console.log(JSON.stringify(contactFormObject));
+  localStorage.setItem('formData', JSON.stringify(contactFormObject));
 });
 
 form.addEventListener('submit', function formSubmitHandler(e) {
